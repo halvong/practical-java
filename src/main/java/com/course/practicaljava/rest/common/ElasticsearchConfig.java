2aa0dee8d6d1a04296eba7a78c74047fd148f625
Hal Vong <halvong@yahoo.com>
2020-08-05 22:25:03 -0700

    elastisearch original gradle2

--- src/main/java/com/course/practicaljava/rest/common/ElasticsearchConfig.java
+++ src/main/java/com/course/practicaljava/rest/common/ElasticsearchConfig.java
@@ -3,7 +3,7 @@ package com.course.practicaljava.rest.common;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 
-@Configuration
-public class ElasticsearchConfig extends AbstractEl{
-}
+//@Configuration
+//public class ElasticsearchConfig extends AbstractEl{
+//}
 
